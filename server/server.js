b0782e43c525746ffeaf240c1ea1064bf7b57380
javadze <jzebrahimi@gmail.com>
2019-03-28 22:26:39 +0430

    fix port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const express = require('express');
 
-const publicPath = path.join(__dirnam,'../public');
+const publicPath = path.join(__dirname,'../public');
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(publicPath));
@@ -23,7 +24,7 @@ app.get('**', (req, res) => {
     res.send('<h1>WTF ARE YOU DOING ???!!<h1>')
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server is runing');
     
 })

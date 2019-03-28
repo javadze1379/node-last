d4927f4ac55e7e11b84ceb0337f222e111ba4a49
javadze <jzebrahimi@gmail.com>
2019-03-28 23:02:46 +0430

    add socket connect an dissconnect event

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const soketIo = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app)
+const io = soketIo(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection' ,(socket) => {
+console.log('new user connected;');
+socket.on('disconnect', ()=>  {
+    console.log('server disconnected');
+    
+})
+})
 // app.use((req,res,next) => {
 //     const log = `${new Date()} ${req.method} ${req.url}`
 //    fs.appendFileSync('server.log', log + '\n')
@@ -14,17 +26,14 @@ app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
     // res.send('<h1>i love this world<h1>')
-    res.send({
-        name: 'javad',
-        duty: 'change your world! spookiely'
-    })
+  
 });
-app.get('**', (req, res) => {
-    // res.send('<h1>i love this world<h1>')
-    res.send('<h1>WTF ARE YOU DOING ???!!<h1>')
-});
+// app.get('**', (req, res) => {
+//     // res.send('<h1>i love this world<h1>')
+//     res.send('<h1>WTF ARE YOU DOING ???!!<h1>')
+// });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('server is runing');
     
 })
